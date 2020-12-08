fc64201e76c01c6a503713e6c8a04ca9ebf58ed2
francoNeira <neira.franco.ariel@gmail.com>
2020-12-08 17:17:40 -0300

    Agregada dependencia de bootstrap y creada home con titulo del juego

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="container-fluid d-flex justify-content-center">
+      <h1>Piedra, Papel, Tijera, Lagarto o Spock</h1>
+    </div>
+  );
+};
+
+export default Home;
